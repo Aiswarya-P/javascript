48b71e917a161f76d2468900029c182ba9d07470
Aiswarya-P <48434365+Aiswarya-P@users.noreply.github.com>
2019-03-11 16:25:23 +0530

    Create vowel-consonent.js

---
+++ vowel-consonent.js
@@ -0,0 +1,8 @@
+#vowel and consonent
+
+var x=prompt("enter a number");
+if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+   document.write("vowel");
+}else{
+  document.write("consonant");
+}
